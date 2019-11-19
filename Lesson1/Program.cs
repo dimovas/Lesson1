e3fea520ead7cd99242e643ff32a3682c404688d
unknown <C:\Users\Sveta\AppData\Roaming\The Bat!>
2019-11-19 11:42:00 +0200

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+       [STAThread]
+        static void Main()
+        {
+            Testing tm = new Testing();
+            Console.WriteLine("Testing.Who Test");
+            tm.WhoTest();
+            Console.WriteLine("Testing.ToString Test");
+            // tm.ToStringTest();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1/Testing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    public class Testing
+    {
+        byte b = 255;
+        int x = 11;
+        uint ux = 1111;
+        float y = 5.5f;
+        double dy = 5.55;
+        string s = "hello!";
+        string s1 = "25";
+        object obj = new Object();
+    
+
+        void WhoIsWho(string name, object any)
+            {
+                Console.WriteLine("type {0} is {1} , value is {2}",name, any.GetType(), any.ToString());
+            }
+
+        public void WhoTest()
+        {
+            WhoIsWho("x", x);
+            WhoIsWho("ux", ux);
+            WhoIsWho("y", y);
+            WhoIsWho("dy", dy);
+            WhoIsWho("s", s);
+            WhoIsWho("11+5.55+5.5f", 11 + 5.55 + 5.5f);
+            obj = 11 + 5.55 + 5.5f;
+            WhoIsWho("obj", obj);
+        }
+    }
+}
+
+
